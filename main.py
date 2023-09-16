d1a77fa5c41ca07f2a9a77e7163e5bde136b173c
Giseldo Neo <giseldo@gmail.com>
2023-09-16 16:55:29 -0300

    ultima versao

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import graphviz
+
+st.write(""" Uma raposa passou embaixo de um pé carregado com lindas uvas. Ficou com muita vontade de comer aquelas uvas. Deu muitos saltos, tentou subir na parreira, mas não conseguiu. Depois de muito tentar foi-se embora, dizendo:
+— Eu nem estou ligando para as uvas. Elas estão verdes, mesmo...""")
+
+txt_protagonista = st.text_input(label="Quem é o protagonista?")
+
+txt_modo = st.text_input(label="Quem é o narrador?")
+
+graph = graphviz.Digraph()
+graph.edge('Sobre a estrutura', "Quem é o protagonista?")
+graph.edge('Sobre a estrutura', "Quem é o narrador?")
+graph.edge('Quem é o protagonista?', txt_protagonista)
+graph.edge('Quem é o narrador?', txt_modo)
+st.graphviz_chart(graph)
+
